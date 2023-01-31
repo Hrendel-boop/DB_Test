caa03c216e5863a5517bd908b40ee601a35a1c4e
giraffeguids@gmail.com <giraffeguids@gmail.com>
2023-01-31 12:19:00 +0600

    прст

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ApplicationContext db= new ApplicationContext())
+            {
+                Console.WriteLine("Напишите имя, а затем возраст");
+                User newUser = new User { Name = Console.ReadLine(), Age = int.Parse(Console.ReadLine())};
+                
+
+                db.Users.Add(newUser);
+                   
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно сохранены");
+
+                var users= db.Users.ToList();
+                Console.WriteLine("Список объeтов:");
+                foreach (User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+
+            }
+
+
+        }
+    }
+}
